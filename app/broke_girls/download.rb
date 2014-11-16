6ff5e69decaecf279d580ba6ed6f0f4d183202da
sleepmoon.jia <sleepmoon.jia@gmail.com>
2014-11-16 15:45:56 +0800

    fix download url

--- app/broke_girls/download.rb
+++ app/broke_girls/download.rb
@@ -1,7 +1,7 @@
 class DownloadSublitle
   class << self
     def download_rar
-      path = File.expand_path("../new_file/session_url.txt", __FILE__)
+      path = File.expand_path("html.txt", __FILE__)
       urls = File.readlines(path)
 
       # urls.each_with_index do |uri, index|
@@ -11,24 +11,21 @@ class DownloadSublitle
     end
 
     def parse_html
-      path = File.expand_path("../new_file/html.txt", __FILE__)
+      path = File.expand_path("html.txt", __FILE__)
       if File.exist?(path)
         return "File already exist"
       end
-      url = 'http://www.yyets.com/search/index?keyword=%E6%89%93%E5%B7%A5%E5%A7%90%E5%A6%B9%E8%8A%B1+%E7%AC%AC%E4%B8%80%E5%AD%A3&type='
+      # url = 'http://www.yyets.com/search/index?keyword=%E6%89%93%E5%B7%A5%E5%A7%90%E5%A6%B9%E8%8A%B1+%E7%AC%AC%E4%B8%80%E5%AD%A3&type='
+      # url = "http://www.yyets.com/search/index?keyword=%E6%89%93%E5%B7%A5%E5%A7%90%E5%A6%B9%E8%8A%B1+%E7%AC%AC%E4%BA%8C%E5%AD%A3&type="
+      url = "http://www.yyets.com/search/index?keyword=%E6%89%93%E5%B7%A5%E5%A7%90%E5%A6%B9%E8%8A%B1+%E7%AC%AC%E4%B8%89%E5%AD%A3&type=subtitle" #3
       response = %x[ curl #{url} ].to_s
-      new_file = File.open(path,"w")
-      new_file.puts response   
+      p1 = response.scan(/http:\/\/www\.yyets\.com\/subtitle\/\d*/)
+      p p1.uniq
+      p p1.size
+      new_file = File.open("html.txt","w")
+      new_file.puts p1.uniq   
     end
 
-    def parse_url
-      path = File.expand_path("../new_file/html.txt", __FILE__)
-      html = File.read(path)
-      download_url = html.scan(/www\.yyets\.com\/subtitle\/\d+/)
-
-      new_file = File.open(File.expand_path("../new_file/download_url.txt", __FILE__),"w")
-      new_file.puts download_url
-    end
 
     def session_url
       path = File.expand_path("../new_file/download_url.txt", __FILE__)
@@ -43,3 +40,4 @@ class DownloadSublitle
     end
   end  
 end
+DownloadSublitle.parse_html
